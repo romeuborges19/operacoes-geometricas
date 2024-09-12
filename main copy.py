229c96dad4691d07792554245e2abc93ed3161a4
Ana Flavia Moreira Pires <flavia.pires@mail.uft.edu.br>
2024-09-12 17:10:29 -0300

    feat: :sparkles: add op aritmetica

---
+++ main copy.py
@@ -0,0 +1,15 @@
+from operacao_aritmetica import OperacaoAritmetica
+
+def main():
+
+    arquivo_matriz = 'matriz.txt'
+    op_aritmetica = OperacaoAritmetica(arquivo_matriz)
+    matrizes_somadas, matrizes_subtraidas = op_aritmetica._operacao_aritmetica()
+    
+
+    op_geomatrica = 
+    # arquivo_saida = 'matrizes.txt'
+    # interpolador.salvar_matrizes_em_arquivo(matrizes_reduzidas, matrizes_ampliadas, arquivo_saida)
+
+if __name__ == "__main__":
+    main()
